b712475fac523d36843115f1c22f1f9f7b5ae59f
atorulia <33317367+atorulia@users.noreply.github.com>
2022-10-10 13:34:06 +0300

    display empty history widget

--- lib/history/view/history_view.dart
+++ lib/history/view/history_view.dart
@@ -1,6 +1,7 @@
 import 'package:cat_trivia/app/view/assets.dart';
 import 'package:cat_trivia/components/animations/bad_cat_animation.dart';
 import 'package:cat_trivia/components/texts/subtitle1_text.dart';
+import 'package:cat_trivia/facts/fact/model/fact.dart';
 import 'package:cat_trivia/history/cubit/history_cubit.dart';
 import 'package:cat_trivia/history/view/components/history_list_view.dart';
 import 'package:flutter/material.dart';
@@ -17,16 +18,27 @@ class HistoryView extends StatelessWidget {
         if (state is HistoryLoading) {
           return _buildLoading();
         } else if (state is HistoryLoaded) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: HistoryListView(facts: state.facts),
-          );
+          return _buildHistory(state.facts);
         } else if (state is HistoryError) {
           return _buildError(state.errorMessage);
         } else {
           return const SizedBox.shrink();
         }
       },
+    );
+  }
+
+  Widget _buildHistory(List<Fact> facts) {
+    if (facts.isEmpty) {
+      return _buildEmptyHistory();
+    } else {
+      return HistoryListView(facts: facts);
+    }
+  }
+
+  Widget _buildEmptyHistory() {
+    return const Center(
+      child: Subtitle1Text('No history yet'),
     );
   }
 
